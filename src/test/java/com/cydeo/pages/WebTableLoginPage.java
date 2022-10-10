fcfc0feecf1e7ce0a6a4470e7e36e9bac30414f2
CYDEO <tubas2334@gmail.com>
2022-10-11 00:39:13 +0300

    updated

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -1,5 +1,6 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,5 +20,41 @@ public class WebTableLoginPage {
 
     @FindBy(xpath = "//button[.='Login']")
     public WebElement loginButton;
+
+    /**
+     * No parameters
+     * When we all this method, it will directly login using
+     * username : Test
+     * Password : Tester
+     *
+     */
+    public void login(){
+        this.inputUsername.sendKeys("Test");
+        this.inputPassword.sendKeys("Tester");
+        this.loginButton.click();
+
+    }
+
+    /**
+     * This method will accept two arguments and login
+     * @param password
+     * @param username
+     */
+    public void login( String username,String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+
+    }
+
+    /**
+     * This method will login using credentials from configuration.properties
+     */
+    public void loginWithConfig(){
+        inputUsername.sendKeys(ConfigurationReader.getProperty("web.table.username"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("web.table.password"));
+        loginButton.click();
+    }
+
 
 }
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -4,8 +4,11 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -58,6 +61,28 @@ public class BrowserUtils {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
         wait.until(ExpectedConditions.invisibilityOf(webElement));
 
+    }
+
+    /**
+     * this method will verify if the current url contains expected value
+      */
+
+    public static void verifyUrlContains(String expectedUrl){
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedUrl));
+
+    }
+
+    public static List<String> dropdownOptionsAsString(WebElement dropdownElement){
+        Select select = new Select(dropdownElement);
+        List<WebElement> actualOptionsAsWebElement = select.getOptions();
+
+        List<String> actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+        return actualOptionsAsString;
     }
 
 
